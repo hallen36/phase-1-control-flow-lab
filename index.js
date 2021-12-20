23282fd3b3b2ff203318ed3106f7be43066748ba
John Hunter Allen <hallen36@gmail.com>
2021-12-20 18:35:41 -0500

    commit tree ab00e97d6f195bf8f6781b31940e7498be155582

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  if(number <= 400) {
+    return 'This one is on me!'
+  } else if (number > 2500) {
+    return 'No can do.'
+  } else if (number > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.' }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(response){
+  let greetings;
+  switch(response){
+  case 'generous':
+        greetings = 'Thank you so much.';
+        break;
+  case 'not as generous':
+        greetings = 'Thank you.';
+        break;
+  default:
+        greetings = 'Bye.';
+      } 
+    return greetings
+  }
